f3c1070d32ba2319de62042d95fb761cbb626645
Alexandre Muniz <aom@puzzlezapper.com>
2019-12-05 15:18:49 -0800

    test for min code point

---
+++ models/unichar.test.js
@@ -0,0 +1,14 @@
+const Unichar = require('./unichar');
+
+describe('Unichar model', () => {
+  it('has a required name', () => {
+    const unichar = new Unichar({});
+    expect(unichar.validateSync().errors.name.message)
+      .toEqual('Path `name` is required.');
+  });
+  it('does not have a code point less than 0', () => {
+    const unichar = new Unichar({ name: 'A', codePoint: -1 });
+    expect(unichar.validateSync().errors.name.message)
+      .toEqual('Path `name` is required.');
+  });
+});
