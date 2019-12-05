934dd33ed84f57c61e46ca820d3ef0163a59e1bc
Alexandre Muniz <aom@puzzlezapper.com>
2019-12-05 15:54:11 -0800

    added required zip

--- models/address.js
+++ models/address.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
-
+  zip: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 5
+  },
 });
 
 module.exports = mongoose.model('Address', schema);
--- models/address.test.js
+++ models/address.test.js
@@ -1,1 +1,9 @@
 const Address = require('./address');
+
+describe('Address model', () => {
+  it('has a required zipcode', () => {
+    const address = new Address({});
+    expect(address.validateSync().errors.zip.message)
+      .toEqual('Path `zip` is required.');
+  });
+});
