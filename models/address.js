03e16ee4bdbf76ec68152617049e432b072e6e84
Alexandre Muniz <aom@puzzlezapper.com>
2019-12-05 16:00:23 -0800

    add street, city, and state to schema

--- models/address.js
+++ models/address.js
@@ -1,6 +1,20 @@
 const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
+  street: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 2
+  },  
   zip: {
     type: String,
     required: true,
--- models/address.test.js
+++ models/address.test.js
@@ -1,6 +1,21 @@
 const Address = require('./address');
 
 describe('Address model', () => {
+  it('has a required street', () => {
+    const address = new Address({});
+    expect(address.validateSync().errors.zip.message)
+      .toEqual('Path `street` is required.');
+  });
+  it('has a required city', () => {
+    const address = new Address({});
+    expect(address.validateSync().errors.zip.message)
+      .toEqual('Path `city` is required.');
+  });
+  it('has a required state', () => {
+    const address = new Address({});
+    expect(address.validateSync().errors.zip.message)
+      .toEqual('Path `state` is required.');
+  });
   it('has a required zipcode', () => {
     const address = new Address({});
     expect(address.validateSync().errors.zip.message)
