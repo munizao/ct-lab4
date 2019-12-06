14e8525e647fc8248b57d359ec17bb29a48396a5
Alexandre Muniz <aom@puzzlezapper.com>
2019-12-05 17:46:36 -0800

    fixed negaative score test

--- models/nba-game.test.js
+++ models/nba-game.test.js
@@ -1,10 +1,16 @@
 const NBAGame = require('./nba-game');
 
 describe('nbaGame model', () => {
+  it('has a tipoff as required', () => {
+    const nbaGame = new NBAGame({ score: [0, 0] });
+    expect(nbaGame.validateSync().errors.tipoff.message)
+      .toEqual('Path `tipoff` is required.');
+  });
+
   it('does not have a negative score', () => {
-    nbaGame = NBAGame({ score: [-1, 0] });
+    const nbaGame = new NBAGame({ tipoff: Date.now(), scores: [-1, 0] });
     expect(nbaGame.validateSync().errors.scores.message)
-    .toEqual('');
+      .toEqual('Cast to Array failed for value "[ -1, 0 ]" at path "scores"');
   });
 
 });
