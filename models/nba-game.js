854665cf0c5e154657b2569008f95df605d9f761
Alexandre Muniz <aom@puzzlezapper.com>
2019-12-05 17:10:20 -0800

    wrote schema for nba game

---
+++ models/nba-game.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const score = new mongoose.Schema({
+  type: Number,
+  min: 0
+});
+
+const schema = new mongoose.Schema({
+  tipoff: { 
+    type: Date,
+    required = true
+  },
+  teams: [String],
+  scores: {
+    type: [score],
+    required: true,
+    maxlength: 2,
+    minlength: 2
+  }
+});
