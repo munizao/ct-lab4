48b95ea74b7629cda14e3cd1943a566d2b7541b3
Alexandre Muniz <aom@puzzlezapper.com>
2019-12-05 16:51:12 -0800

    added tests for title and performer

--- models/mp3.test.js
+++ models/mp3.test.js
@@ -1,5 +1,24 @@
-const mp3 = require('./mp3');
+const MP3 = require('./mp3');
 
 describe('MP3 model', () => {
-
+  it('has required title', () => {
+    const mp3 = new MP3({});
+    expect(mp3.validateSync().errors.title.message)
+    .toEqual('Path `title` is required.');
+  });
+  it('has required performer', () => {
+    const mp3 = new MP3({});
+    expect(mp3.validateSync().errors.performer.message)
+    .toEqual('Path `performer` is required.');
+  });
+  it('has a track number that is not too small', () => {
+    const mp3 = new MP3({ track: -1 });
+    expect(mp3.validateSync().errors.track.message)
+      .toEqual('Path `track` (-1) is less than minimum allowed value (1).');
+  });
+  it('has a track number that is not too large', () => {
+    const mp3 = new MP3({ track: 1000 });
+    expect(mp3.validateSync().errors.track.message)
+      .toEqual('Path `track` (1000) is more than maximum allowed value (99).');
+  });
 });
