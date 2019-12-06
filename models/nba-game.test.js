9f3519c178113232645e350765231fe9033afe8c
Alexandre Muniz <aom@puzzlezapper.com>
2019-12-05 17:17:38 -0800

    added test for negative score

---
+++ models/nba-game.test.js
@@ -0,0 +1,10 @@
+const NBAGame = require('./nba-game');
+
+describe('nbaGame model', () => {
+  it('does not have a negative score', () => {
+    nbaGame = NBAGame({ score = [-1, 0] });
+    expect(nbaGame.validateSync().errors.scores.message)
+    .toEqual('');
+  });
+
+});
